5f88fe333f699da30b785b27eff93d20d21d49b5
金明 <mooser@kintekiMacBook-Pro.local>
2013-07-17 22:15:23 +0800

    add install module script

---
+++ script/install_module.pl
@@ -0,0 +1,61 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use Data::Dumper;
+
+my $cpanm_path = shift;
+
+$cpanm_path = qx{which cpanm};
+chomp($cpanm_path);
+my $usage =<<EOF;
+$0 /usr/sbin/cpanm
+EOF
+
+if( not $cpanm_path ){
+    die "please defined you cpanm_path\n please input as the example: $usage";
+}
+my @module_list = qw(
+   AnyEvent
+   AnyEvent::HTTP
+   Coro
+   DBD::SQLite
+   DBD::mysql
+   DBIx::Class
+   DBIx::Class::Schema::Loader
+   Data::Dump
+   Date::Time
+   DateTime
+   DateTime::Format::Builder
+   DateTime::Format::DateParse
+   DateTime::Format::ISO8601
+   DateTime::Format::Mail
+   DateTime::Format::RSS
+   DateTime::Format::Strptime
+   DateTime::Format::W3CDTF
+   DateTime::TimeZone
+   DateTime::Tiny
+   EV
+   JSON
+   JSON::XS
+   LWP
+   List::Util
+   Mojolicious
+   MongoDB
+   Moo
+   MooseX::MarkAsMethods
+   Mouse
+   Object::Event
+   Parallel::ForkManager
+   TryCatch
+   URL::Encode
+   WordPress
+   WordPress::API
+   WordPress::XMLRPC
+   YAML
+   namespace::clean
+);
+
+my $pre = "$cpanm_path";
+system($pre,$_) for @module_list;
+
+

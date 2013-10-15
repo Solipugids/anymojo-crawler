c6ba3dc44e989763f15d4741c5e3c5bc91edea0c
舌尖上的牛氓 <yiming.jin@live.com>
2013-10-15 18:45:16 +0800

    del mongo

--- lib/Crawler/DB.pm
+++ lib/Crawler/DB.pm
@@ -1,13 +1,9 @@
 package Crawler::DB;
 
 use Moo;
-use Crawler::DB::Mongo;
 
 sub connect_db{
     my ($self,$data_source,$dsn,$user,$passwd) = @_;
-    if( $data_source eq 'mongo'){
-        return Crawler::DB::Mongo->new( $dsn );
-    }
 }
 
 
